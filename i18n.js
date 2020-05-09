6a5dee5b5fe8a98d7878235ae79163e9cc4802e5
melih <mlhclskn@gmail.com>
2020-05-09 19:32:05 +0300

    add localepath

--- i18n.js
+++ i18n.js
@@ -3,6 +3,7 @@ const NextI18Next = require('next-i18next').default
 module.exports = new NextI18Next({
     defaultLanguage: "tr",
     otherLanguages: ['en'],
+    localePath:"public/static/locales",
     localeSubpaths: {
         tr: 'tr',
         en: 'en'
