8cb16226244251dfd3ffd471fa8123bf95d37572
melih <mlhclskn@gmail.com>
2020-05-27 01:38:41 +0300

    added new icons

---
+++ components/svg/Plus.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="25"
+      fill="none"
+      viewBox="0 0 24 25"
+    >
+      <path
+        fill="#000"
+        fillRule="evenodd"
+        d="M13 11.5h9v2h-9v9h-2v-9H2v-2h9v-9h2v9z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
---
+++ components/svg/Search.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Icon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="25"
+            fill="none"
+            viewBox="0 0 24 25"
+        >
+            <path
+                fill="#000"
+                fillRule="evenodd"
+                d="M10 18.5a8 8 0 116.32-3.094l5.387 5.387-1.414 1.414-5.387-5.387A7.965 7.965 0 0110 18.5zm6-8a6 6 0 11-12 0 6 6 0 0112 0z"
+                clipRule="evenodd"
+            ></path>
+        </svg>
+    );
+}
+
+export default Icon;
