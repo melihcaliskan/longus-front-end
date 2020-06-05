7257a69b725b0bc71417bae304f368a4e693cb46
melih <mlhclskn@gmail.com>
2020-06-05 20:53:30 +0300

    removed comment lines

--- contexts/useDarkMode.js
+++ contexts/useDarkMode.js
@@ -13,17 +13,6 @@ export const useDarkMode = () => {
     theme === 'light' ? setMode('dark') : setMode('light')
   };
 
-  /*
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem('theme');
-    window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && !localTheme ?
-      setMode('dark') :
-      localTheme ?
-        setTheme(localTheme) :
-        setMode('light');
-    setComponentMounted(true);
-  }, []);
-  */
   useEffect(() => {
     const localTheme = window.localStorage.getItem('theme');
     if (localTheme) {
