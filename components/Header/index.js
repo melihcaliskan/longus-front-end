51429a17fb0172c65fbbb85a921343091fa498cf
melih <mlhclskn@gmail.com>
2020-06-05 23:04:22 +0300

    fixed detail page color

--- components/Header/index.js
+++ components/Header/index.js
@@ -87,12 +87,12 @@ const Container = styled.div`
   
   ${Left}{
     ${Title}, a, p{
-      color:${props => props.isCollapsed ? props.theme.title : props.theme.body_700};
+      color:${props => props.isCollapsed ? props.theme.title : props.theme.dark_text_200};
     }
   }
   ${Right}{
     svg, path{
-      fill:${props => props.isCollapsed ? props.theme.title : props.theme.body_700};
+      fill:${props => props.isCollapsed ? props.theme.title : props.theme.dark_text_300};
     }
   }
 
