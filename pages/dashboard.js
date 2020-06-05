8e3d63fd97115687b169f40ea8d89eee1d4683cb
melih <mlhclskn@gmail.com>
2020-06-05 20:26:37 +0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,12 @@
 import ActiveLink from '../components/ActiveLink'
+import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import { FindButton } from '../components/Home/Header'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Loader from '../helpers/Loader'
 import React from 'react'
+import Row from 'react-bootstrap/Row'
 import styled from 'styled-components';
 import { useRouter } from 'next/router'
 import { withTranslation } from '../i18n'
@@ -21,61 +23,36 @@ const FourZeroFour = styled.h1`
 `
 
 const CustomContainer = styled.div`
-    margin-top:4em;
-    margin-bottom:15em;
-    display:flex;
-    align-items:flex-start;
-    justify-content:space-between;
-
-    h3{
-      font-size:18px;
-    }
-    .no-desktop{
-      display:none;
-    }
+    margin-top:5em;
     @media only screen and (max-width: 960px) {
-      margin-top:2em;
-      .no-desktop{
-        display:initial;
-        margin-bottom:2em;
-      }
-      .no-mobile{
-        display:none;
-      }
-      align-items:center;
-      flex-direction:column;
-      text-align:center;
     }
 `
 
 const Dashboard = ({ isAuth, jwt, t, tReady, isLight, toggleTheme, theme, language }) => {
   const router = useRouter()
-
   if (!isAuth) {
     router.push("/")
     return <Loader />
   }
-  const test = () => {
-    localStorage.setItem("jwt", "140px");
-    localStorage.setItem("USER_DATA", "345");
-  }
-  const twotest = () => {
-    localStorage.removeItem("jwt");
-    localStorage.removeItem("USER_DATA");
-  }
+
   return (
     <>
-      <Header isLight={isLight} theme={theme} toggleTheme={toggleTheme} />
+      <Header isLight={isLight} theme={theme} toggleTheme={toggleTheme} noNav />
       <CustomContainer as={Container}>
-        <img className="no-desktop" src="/assets/404.svg" width={200} />
-      welcome to dashboard {jwt}
+        <Row>
+          <Col>
+            left menu
+          </Col>
+          <Col xs={6}>
+            <h1>Akış</h1>
+            <img className="no-desktop" src="/assets/404.svg" width={200} />
+            <p> welcome to dashboard {jwt}</p>
+          </Col>
+          <Col>
+            right menu
+          </Col>
+        </Row>
       </CustomContainer>
-      <button onClick={test}>
-        add
-      </button>
-      <button onClick={twotest}>
-        remove
-      </button>
       <Footer />
     </>
   )
