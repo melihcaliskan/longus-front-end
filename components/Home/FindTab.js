0ad19863cf1f030cd5e669baa03325d95fe8963a
melih <mlhclskn@gmail.com>
2020-06-09 02:05:25 +0300

    Update FindTab.js

--- components/Home/FindTab.js
+++ components/Home/FindTab.js
@@ -228,7 +228,7 @@ const List = ({ t, data, count }) => {
                 .sort((a, b) => a.sort - b.sort)
                 .map((a) => a.value)
                 .slice(0, count).map((item, index) => (
-                    <ActiveLink key={item.id} href={"/device/1"} query={{ brand: item.brand, name: item.name }}>
+                    <ActiveLink key={item.id} href={"/devices/1"} query={{ brand: item.brand, name: item.name }}>
                         <Item white={index % 2 == 0} last={data.slice(0, count).length == index + 1}>
                             <Body>
                                 <Top>
